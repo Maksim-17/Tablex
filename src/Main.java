d15fc1fdbf21601d1840c25ce87832c8d80db647
Maxim <dino1786@mail.ru>
2019-10-30 23:08:38 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+//Необходимо вывести на экран таблицу умножения от 1 до 10 для введённого пользователем любого числа. Использовать циклы запрещено
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+            int q = 1;
+            int w = 2;
+            int e = 3;
+            int r = 4;
+            int t = 5;
+            int y = 6;
+            int u = 7;
+            int i = 8;
+            int o = 9;
+            int p = 10;
+
+            System.out.println(A * q);
+            System.out.println(A * w);
+            System.out.println(A * e);
+            System.out.println(A * r);
+            System.out.println(A * t);
+            System.out.println(A * y);
+            System.out.println(A * u);
+            System.out.println(A * i);
+            System.out.println(A * o);
+            System.out.println(A * p);
+
+
+
+            }
+
+        }
